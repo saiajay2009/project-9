376d39ed520c0c1eb5a59316888e9f434cec9a4a
saiajay2009 <97821371+saiajay2009@users.noreply.github.com>
2022-01-29 22:22:35 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+
+  
+createCanvas(5000, 1000);
+{
+
+ background("yellow");
+}   
+}
+function draw()
+{
+   
+   if  (keyIsDown(RIGHT_ARROW)){
+background("red");
+   }
+
+   if (keyIsDown(LEFT_ARROW)){
+      background("yellow");
+   }
+   
+}
